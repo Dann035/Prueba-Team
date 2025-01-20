4709d1aa1efb6a982574b9149c9c3b328010c220
Z320 C001 <landadlh603@gmail.com>
2025-01-20 19:22:59 +0100

    agregando funcionalidad con el archivo app.js

--- src/app.js
+++ src/app.js
@@ -17,37 +17,66 @@ function saludar(){
     }else {
         alert(`Necesito tu nombre para saludarte, no te voy a mostar la pagina😢`);
         alert(`Refresca la pagina y pon tu nombre o no te neseño la pagina 😑`);
-        document.querySelector('body').style.display = 'none';
     }
 }
 
-function cambiarColorFondo (btnClass,color){
-    document.querySelector(btnClass).addEventListener('click',function (){
+function cambiarColorFondo (btnClass,color,colorFont){
+    const btn = document.querySelector(btnClass);
+    if(btn){
+        btn.addEventListener('click',function (){
         containerTitle.style.backgroundColor = color;
+        containerTitle.style.color = colorFont;
         postList.style.backgroundColor = color;
-    });
-};
+        postList.style.color = colorFont;
+        })
+    }
+}
 
-cambiarColorFondo('.btn-bg-red','red');
-cambiarColorFondo('.btn-bg-yellow','yellow');
-cambiarColorFondo('.btn-bg-green','green');
-cambiarColorFondo('.btn-bg-cyan','cyan');
+cambiarColorFondo('.btn-bg-red','red','white');
+cambiarColorFondo('.btn-bg-yellow','yellow','black');
+cambiarColorFondo('.btn-bg-green','green','white');
+cambiarColorFondo('.btn-bg-cyan','cyan','black');
+cambiarColorFondo('.house','rgb(217, 217, 217)','black');
 
 // saludar()
 // Agregar funcionas fuera del cambio de color que tengo encima
+btnAdd.addEventListener('click', () => {
+    try {
+        let textInputAdd = inputAdd.value.trim();
+        if (textInputAdd !== ''){
+            if(list){
+                let newLi = document.createElement('li');
+                newLi.textContent = textInputAdd;
+                list.appendChild(newLi);
+                let btnDel = document.createElement('button');
+                btnDel.setAttribute('class', 'btnDel');
+                newLi.appendChild(btnDel);
+                inputAdd.value = '';
+                btnDel.addEventListener('click', () => {
+                    newLi.remove();
+                })
+            } else {
+                console.error('No se encontro el elemento .list');
+            }
+        }
+    } catch (error) {
+        console.log('Error en la parte de cracion de elementos de la lista',error)
+    } 
+});
 
-function agregarElemtList(){
-    let textInputAdd = inputAdd.value.trim();
-    if (textInputAdd !== ''){
-        let newLi = document.createElement('li');
-        newLi.textContent = textInputAdd;
-        list.appendChild(newLi);
-        let btnDel = document.createElement('button');
-        btnDel.setAttribute('class', 'btnDel');
-        btnDel.textContent = 'DEL';
-        list.appendChild(btnDel);
-    }else {
-        console.log('El input esta vacio');
+btnFind.addEventListener('click', () => {
+    let textInputFind = inputFind.value.trim();
+    let li = list.getElementsByTagName('li');
+    if (textInputFind !== ''){
+        for (let i = 0; i < li.length; i++) {
+            if (li[i].textContent.toLowerCase().includes(textInputFind.toLowerCase())) {
+                li[i].classList.remove('hidden')
+            }else{
+                li[i].classList.add('hidden')
+                console.log('No encontrado');
+            }
+        }
+    } else {
+        for (let i = 0; i < li.length; i++) {li[i].classList.remove('hidden')}
     }
-}
-
+})
