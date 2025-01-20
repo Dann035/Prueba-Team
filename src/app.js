12068f129b226281d958c96798dd0af4b810ce1c
Z320 C001 <landadlh603@gmail.com>
2025-01-20 14:45:46 +0100

    agregando funcionalidad al btnAdd

--- src/app.js
+++ src/app.js
@@ -2,18 +2,18 @@
 
 const containerTitle = document.querySelector('.container-title');
 const postList = document.querySelector('.post-list');
-const list = document.querySelectorAll('.list');
+const list = document.querySelector('.list');
 const inputAdd = document.querySelector('.inputAdd');
 const inputFind = document.querySelector('.inputFind');
 const btnAdd = document.querySelector('.btnAdd');
-const btnFind = document.querySelector('.btn-Find');
+const btnFind = document.querySelector('.btnFind');
 
 // Agragar funciones a los botones de color
 
 function saludar(){
     let nombre = prompt('Como te llamas ? :')
     if(nombre !== ""){
-        alert(`Hola!!,Bienvenido a mi To-do-list ${nombre}`);
+        alert(`Hola!!,Bienvenido a mi To-do-list ${nombre.toUpperCase()}`);
     }else {
         alert(`Necesito tu nombre para saludarte, no te voy a mostar la pagina😢`);
         alert(`Refresca la pagina y pon tu nombre o no te neseño la pagina 😑`);
@@ -33,10 +33,21 @@ cambiarColorFondo('.btn-bg-yellow','yellow');
 cambiarColorFondo('.btn-bg-green','green');
 cambiarColorFondo('.btn-bg-cyan','cyan');
 
-saludar()
+// saludar()
 // Agregar funcionas fuera del cambio de color que tengo encima
 
-btnAdd.addEventListener('click',() => {
-
-})
+function agregarElemtList(){
+    let textInputAdd = inputAdd.value.trim();
+    if (textInputAdd !== ''){
+        let newLi = document.createElement('li');
+        newLi.textContent = textInputAdd;
+        list.appendChild(newLi);
+        let btnDel = document.createElement('button');
+        btnDel.setAttribute('class', 'btnDel');
+        btnDel.textContent = 'DEL';
+        list.appendChild(btnDel);
+    }else {
+        console.log('El input esta vacio');
+    }
+}
 
